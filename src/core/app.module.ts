cf8911d12d11846f951468004dd3c6bceae9864f
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-09-13 17:25:42 +0300

    Prepare Docker

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -7,7 +7,14 @@ import { BookingModule } from "@/modules/booking/booking.module";
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({ isGlobal: true }),
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: [
+				`.env.${process.env.NODE_ENV}.local`,
+				`.env.${process.env.NODE_ENV}`,
+				".env",
+			],
+		}),
 		LoggerModule.forRoot(),
 		DatabaseModule,
 		BookingModule,
